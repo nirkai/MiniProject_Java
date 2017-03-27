679f48bb942a123ddafe018409961df06dbd915a
DESKTOP-F9EM8B2\אוריה <5382448@gmail.com>
2017-03-27 23:24:57 +0300

    ray class

---
+++ src/primitives/Ray.java
@@ -0,0 +1,37 @@
+package primitives;
+
+public class Ray {
+	// Point of origin
+	private Point3D _POO;
+	// Ray direction
+	private Vector _direction;
+	// ***************** Constructors ********************** //
+	public Ray(){
+		_POO=new  Point3D();
+		_direction=new Vector();
+	}
+	
+	public Ray(Ray ray){
+		_POO = new Point3D(ray._POO);
+		_direction=new Vector(ray._direction);
+		
+	}
+	public Ray(Point3D poo, Vector direction){
+		_POO=new Point3D(poo);
+		_direction=new Vector(direction);
+	}
+	// ***************** Getters/Setters ********************** //
+	public void setPOO(Point3D _POO){
+		_POO=new Point3D(_POO);
+	}
+	public void setDirection(Vector _direction){
+		_direction=new Vector(_direction);
+	}
+	public Vector getDirection(){
+		return _direction;
+	}
+	public Point3D getPOO(){
+		return _POO;
+	}
+
+}
