7bd0b94d4e394af4fb4de7964b51b18707feaf50
n k <kaizler@g.jct.ac.il>
2017-05-14 13:41:22 +0300

    update

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -3,16 +3,19 @@ package elements;
 import primitives.*;
 import java.awt.Color;
 
-public class SpotLight {
+public class SpotLight extends PointLight {
 	private Vector _direction;
 	// ***************** Constructor ********************** //
 	public SpotLight(Color color, Point3D position, Vector direction,
 	double kc, double kl, double kq){
-		
+		super(color, position, kc, kl, kq);
+		_direction = new Vector(direction);
 	}
 	// ***************** Getters/Setters ********************** //
 	public Color getIntensity(Point3D point){
 		// TODO
+		double d = _position.distance(point);
+		//_direction.dotProduct(_direction)
 		return null;
 	}
 }
