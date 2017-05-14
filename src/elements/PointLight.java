43cef6be31c4f1f180d357454ad69324ed2f55cb
DESKTOP-F9EM8B2\אוריה <5382448@gmail.com>
2017-05-14 12:30:40 +0300

    Class PointLight

---
+++ src/elements/PointLight.java
@@ -0,0 +1,26 @@
+package elements;
+
+import primitives.*;
+import java.awt.Color;
+
+public class PointLight extends Light{
+	Point3D _position;
+	double _Kc, _Kl, _Kq;
+	// ***************** Constructors ********************** //
+	public PointLight(Color color, Point3D position,
+	double kc, double kl, double kq){
+		super(color);
+		_position = new Point3D(position);
+		_Kc = kc;
+		_Kl = kl;
+		_Kq = kq;
+	}
+	// ***************** Getters/Setters ********************** //
+	public Color getIntensity(Point3D point)
+	{
+		return super.getIntensity();
+	}
+	public Vector getL(Point3D point){
+		return null;
+	}
+}
